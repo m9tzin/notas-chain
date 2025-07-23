f5a21333c7e155aa0ec65b7fff406b653582ca53
m9tzin <matheusmrno@gmail.com>
2025-07-23 15:31:41 -0300

    feat. new chaincode changes

--- chaincode/notas/nota.go
+++ chaincode/notas/nota.go
@@ -3,23 +3,44 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
+/* Nota é a estrutura de dados que representa uma nota de um aluno em uma disciplina */
+type Nota struct {
+	ID         string  `json:"id"`
+	AlunoID    string  `json:"aluno_id"`
+	Disciplina string  `json:"disciplina"`
+	Nota       float64 `json:"nota"`
+	Data       string  `json:"data"`
+	Timestamp  string  `json:"timestamp"`
+}
+
+/* NotaContract é o contrato que define as funções do chaincode */
 type NotaContract struct {
 	contractapi.Contract
 }
 
-type Nota struct {
-	ID         string  `json:"id"`
-	Aluno      string  `json:"aluno"`
-	Disciplina string  `json:"disciplina"`
-	Nota       float64 `json:"nota"`
-	Data       string  `json:"data"`
+func (s *NotaContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
+	notas := []Nota{
+		{ID: "1", AlunoID: "1", Disciplina: "Matemática", Nota: 8.5, Data: "2025-11-11", Timestamp: time.Now().Format(time.RFC3339)},
+	}
+	for _, nota := range notas {
+		bytes, err := json.Marshal(nota)
+		if err != nil {
+			return err
+		}
+		err = ctx.GetStub().PutState(nota.ID, bytes)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-func (c *NotaContract) CreateNota(ctx contractapi.TransactionContextInterface, id, aluno, disciplina, data string, nota float64) error {
+func (c *NotaContract) CreateNota(ctx contractapi.TransactionContextInterface, id, alunoID, disciplina, data string, nota float64) error {
 	exists, err := c.NotaExists(ctx, id)
 	if err != nil {
 		return err
@@ -28,7 +49,7 @@ func (c *NotaContract) CreateNota(ctx contractapi.TransactionContextInterface, i
 		return fmt.Errorf("a nota %s já existe", id)
 	}
 
-	n := Nota{ID: id, Aluno: aluno, Disciplina: disciplina, Nota: nota, Data: data}
+	n := Nota{ID: id, AlunoID: alunoID, Disciplina: disciplina, Nota: nota, Data: data, Timestamp: time.Now().Format(time.RFC3339)}
 	bytes, err := json.Marshal(n)
 	if err != nil {
 		return err
@@ -53,7 +74,7 @@ func (c *NotaContract) ReadNota(ctx contractapi.TransactionContextInterface, id 
 	return &nota, nil
 }
 
-func (c *NotaContract) UpdateNota(ctx contractapi.TransactionContextInterface, id string, aluno string, disciplina string, data string, nota float64) error {
+func (c *NotaContract) UpdateNota(ctx contractapi.TransactionContextInterface, id string, alunoID string, disciplina string, data string, nota float64) error {
 	notaJSON, err := ctx.GetStub().GetState(id)
 	if err != nil {
 		return fmt.Errorf("erro ao buscar nota: %v", err)
@@ -62,12 +83,17 @@ func (c *NotaContract) UpdateNota(ctx contractapi.TransactionContextInterface, i
 		return fmt.Errorf("nota com ID %s não encontrada", id)
 	}
 
+	// Recupera o timestamp anterior, se existir
+	var notaAntiga Nota
+	_ = json.Unmarshal(notaJSON, &notaAntiga)
+
 	notaAtualizada := Nota{
 		ID:         id,
-		Aluno:      aluno,
+		AlunoID:    alunoID,
 		Disciplina: disciplina,
 		Nota:       nota,
-		Data:       data, // Adicionado campo Data
+		Data:       data,
+		Timestamp:  time.Now().Format(time.RFC3339),
 	}
 
 	notaBytes, err := json.Marshal(notaAtualizada)
